b71500c3d7fc636efcd40ecf4a492e4ddc8841dd
VEERA SWAROOPA PATHAKOTA <53139653+roopav16@users.noreply.github.com>
2020-02-10 20:26:57 +0530

    Add files via upload

---
+++ Cal.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+/**
+ * @author Swaroopa
+ *
+ */
+package calculator;
+public class Cal
+{
+	public void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void sub(int a,int b)
+	{
+		System.out.println(a-b);
+	}
+	public void mul(int a,int b)
+	{
+		System.out.println(a*b);
+	}
+	public void div(int a,int b)
+	{
+		System.out.println((float)a/b);
+	}
+}
